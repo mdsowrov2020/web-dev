198b7707e91d1ee721b7efd279162e55b2c64e6b
Antu <prince.anto97@gmai.com>
2020-06-29 21:45:55 +0600

    Updates

---
+++ precache-manifest.efb0215faf777b044299437fbfd9375e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6fe172040d61b8e359f997a8ea1c756",
+    "url": "/web-dev/index.html"
+  },
+  {
+    "revision": "acefb859383642700969",
+    "url": "/web-dev/static/css/main.5fb469c6.chunk.css"
+  },
+  {
+    "revision": "56189ecb51ebc9a56a44",
+    "url": "/web-dev/static/js/2.dd5ade11.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/web-dev/static/js/2.dd5ade11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acefb859383642700969",
+    "url": "/web-dev/static/js/main.6a3e40c3.chunk.js"
+  },
+  {
+    "revision": "53607324f102258c8739",
+    "url": "/web-dev/static/js/runtime-main.4135409b.js"
+  },
+  {
+    "revision": "a7482d2251a3a36c7ebcee791836feb0",
+    "url": "/web-dev/static/media/data-science.a7482d22.png"
+  },
+  {
+    "revision": "5731ccf486f14fc79ed55c181a454572",
+    "url": "/web-dev/static/media/dm.5731ccf4.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/web-dev/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3777552662e3614d0d0efa421a48654b",
+    "url": "/web-dev/static/media/model-1.37775526.jpeg"
+  },
+  {
+    "revision": "33f060f7d259bc9410ceb06def22f2c8",
+    "url": "/web-dev/static/media/seo.33f060f7.png"
+  }
+]);
